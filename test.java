f77a8330ccc5f92756d9b7ea4fe1c04b7bf24d6d
dbhushan18 <57983786+dbhushan18@users.noreply.github.com>
2025-01-09 11:59:31 +0530

    Create test.java
    add test.java

---
+++ test.java
@@ -0,0 +1,9 @@
+#import java.util.*;
+
+public class test{
+        public static void main(String args[]){
+                n1=14;
+                n2=45;
+                System.out.println("addition is: "+(n1+n2));
+}
+}
